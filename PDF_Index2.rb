b963b7b49140ee9d87c9fa11e0571329c43d5594
Marta Kosmala <marta.kos1230@gmail.com>
2018-03-09 22:05:18 +0100

    Poprawki2

--- PDF_Index2.rb
+++ PDF_Index2.rb
@@ -1,20 +1,12 @@
 def find_index(array, value)
-  x = 0
-  y = array.length - 1
 
-  while y >= x
-    if x = value
-      result = x
-      puts result
-      exit(0)
-    else
-      x = x + 1
-    end
+if array.index(value).nil?
+    return "Not found"
+  else
+    return "#{value}"
   end
-
-  result = "not found"
-  puts result
 end
+
 
 puts find_index([1, 2, 3, 4, 5], 3) #2
 puts find_index([5, 8, 6, 2, 2, 10], 2) #3
